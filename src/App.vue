2fd8a31b325441d53e20add705493bfa7e6290ef
thay123028 <thay123028@naver.com>
2025-08-05 17:36:59 +0900

    [feat] 헤더에 필요한 슬라이드창 추가 및 동작 구현

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,43 @@ import { useRouter } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import { useUserStore } from './store/useUserStore'
+import HeaderMenuSlider from './components/HeaderMenuSlider.vue'
+import HeaderSearchSlider from './components/HeaderSearchSlider.vue'
+import { ref } from 'vue'
+
 const router = useRouter()
 const auth = useUserStore()
 auth.restore()
+
+// 메뉴 열림 상태
+const isMenuOpen = ref(false)
+const isSearchSlideOpen = ref(false)
+
+function openMenu() {
+  isMenuOpen.value = true
+}
+
+function closeMenu() {
+  isMenuOpen.value = false
+}
+
+function openSearchSlide() {
+  isSearchSlideOpen.value = true
+}
+
+function closeSearchSlide() {
+  isSearchSlideOpen.value = false
+}
 </script>
 
 <template>
-  <Header v-if="!router.currentRoute.value.meta?.hideLayout" />
+  <Header
+    v-if="!router.currentRoute.value.meta?.hideLayout"
+    @open-menu="openMenu"
+    @open-search-slide="openSearchSlide"
+  />
+  <HeaderMenuSlider v-if="isMenuOpen" @close="closeMenu" />
+  <HeaderSearchSlider v-if="isSearchSlideOpen" @search-close="closeSearchSlide" />
   <RouterView />
   <Footer v-if="!router.currentRoute.value.meta?.hideLayout" />
 </template>

0dca7690ae2e10f083c8e1acea770066153d6196
thay123028 <thay123028@naver.com>
2025-09-02 16:25:32 +0900

    충돌 해결2

--- src/views/PaymentComplete.vue
+++ src/views/PaymentComplete.vue
@@ -26,28 +26,28 @@ const goToShopping = () => {
 }
 
 function goOrderDetail() {
-  const pathParts = route.path.split('/');
-  const orderId = pathParts[pathParts.length - 1];
-  router.push(`/order/details/${orderId}`);
+  const pathParts = route.path.split('/')
+  const orderId = pathParts[pathParts.length - 1]
+  router.push(`/order/details/${orderId}`)
 }
 </script>
 
 <template>
-  <div class="payment-complete-page-body">
   <div class="payment-complete-page-body">
     <div class="payment-complete-contents-container">
       <div>
         <div ref="lottieContainer" class="lottie-image"></div>
         <h3>주문이 완료되었습니다.</h3>
         <p>고객님의 <span>맛있는 한 끼</span>를 위해 빠르게 배송해드릴게요.</p>
-      <div>
-        <div ref="lottieContainer" class="lottie-image"></div>
-        <h3>주문이 완료되었습니다.</h3>
-        <p>고객님의 <span>맛있는 한 끼</span>를 위해 빠르게 배송해드릴게요.</p>
+        <div>
+          <div ref="lottieContainer" class="lottie-image"></div>
+          <h3>주문이 완료되었습니다.</h3>
+          <p>고객님의 <span>맛있는 한 끼</span>를 위해 빠르게 배송해드릴게요.</p>
 
-        <div class="payment-complete-buttons-container">
-          <button @click="goOrderDetail" class="order-detail-page-go">주문 상세보기</button>
-          <button @click="goToShopping" class="shopping-continue-go">쇼핑 계속하기</button>
+          <div class="payment-complete-buttons-container">
+            <button @click="goOrderDetail" class="order-detail-page-go">주문 상세보기</button>
+            <button @click="goToShopping" class="shopping-continue-go">쇼핑 계속하기</button>
+          </div>
         </div>
       </div>
     </div>

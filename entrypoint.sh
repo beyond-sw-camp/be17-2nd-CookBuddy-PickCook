db7dfe379f8fa70a60f34cad058750cae2ed052f
thay123028 <thay123028@naver.com>
2025-09-17 17:24:34 +0900

    -

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,15 @@
 
 # JS 파일 API URL 치환
 if [ -n "$VITE_API_URL" ]; then
+  echo "Replacing Vue API URL with $VITE_API_URL"
   sed -i "s|http://default-api|$VITE_API_URL|g" /usr/share/nginx/html/assets/*.js
 fi
 
-# Nginx proxy_pass 치환
+# Nginx proxy_pass URL 치환
 if [ -n "$BACKEND_URL" ]; then
-  sed -i "s|__API_URL__|$BACKEND_URL|g" /etc/nginx/conf.d/default.conf
+  echo "Replacing Nginx backend URL with $BACKEND_URL"
+  sed -i "s|http://spring-svc.hsy.svc.cluster.local:8080|$BACKEND_URL|g" /etc/nginx/conf.d/default.conf
 fi
 
+# Nginx 시작
 nginx -g "daemon off;"
